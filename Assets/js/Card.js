51362110ef2c9e4642751aa4adc11ff0a707d124
yousefkmail <yousef.hnaty@gmail.com>
2024-06-26 11:32:27 +0300

    add: half star

--- Assets/js/Card.js
+++ Assets/js/Card.js
@@ -2,18 +2,22 @@ export const CreateCard = ({ image, category, rating, name, topic, id }) => {
   const GetStars = (rating) => {
     let stars = document.createElement("div");
     stars.classList.add("stars-container");
-    let i = 0;
+    let i = 1;
     for (; i < rating; i++) {
       let fullStar = document.createElement("span");
-      fullStar.classList.add("fa");
-      fullStar.classList.add("fa-star");
+      fullStar.classList.add("fa", "fa-star");
       stars.appendChild(fullStar);
     }
-    for (; i < 5; i++) {
-      let emptyStar = document.createElement("span");
-      emptyStar.classList.add("fa");
-      emptyStar.classList.add("fa-star-o");
-      stars.appendChild(emptyStar);
+    for (; i <= 5; i++) {
+      if (rating > i - 1) {
+        let emptyStar = document.createElement("span");
+        emptyStar.classList.add("fa", "fa-star-half-o", "color-orange");
+        stars.appendChild(emptyStar);
+      } else {
+        let emptyStar = document.createElement("span");
+        emptyStar.classList.add("fa", "fa-star-o");
+        stars.appendChild(emptyStar);
+      }
     }
     return stars;
   };
