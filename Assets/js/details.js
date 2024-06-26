bc980e270937448d5bcd32be6a4600d6868eda1f
yousefkmail <yousef.hnaty@gmail.com>
2024-06-26 11:43:19 +0300

    feat:add docs to the details.js

--- Assets/js/details.js
+++ Assets/js/details.js
@@ -1,15 +1,19 @@
 import data from "../../topics.json" with { type: "json" };
 import { SubTopic } from "./Subtopic.js";
 import { ToggleThemeMode } from "./Darkmode.js";
+
+//Add Dark mode toggle functionality to its respective button in the details page.
 document.getElementById("theme-toggle").addEventListener("click" , ToggleThemeMode)
 
-const searchParams = new URLSearchParams(window.location.search);
 
+//Retrieve the id from the QueryString in the URL
+const searchParams = new URLSearchParams(window.location.search);
 let id = searchParams.get("id");
 
+//Retrieve the topic the details page is inspecting.
 let detailed_item = data.find((x)=> x.id.toString() === id.toString())
-console.log(detailed_item);
 
+// populate topic data among different sections of the details page.
 document.getElementById("category").innerHTML = detailed_item.category ;
 document.getElementById("topic").innerHTML = detailed_item.topic ;
 document.getElementById("preview-image").src = `./Assets/Logos/${detailed_item.image}` ;
@@ -17,12 +21,10 @@ document.getElementById("auther").innerHTML = detailed_item.name ;
 document.getElementById("description").innerHTML = detailed_item.description ;
 document.getElementById("sub-topics-label").innerHTML = detailed_item.topic + " sub topics";
 
-
+//populate the subtopics 
 let subtopics_container =document.getElementById("subtopics-container");
 subtopics_container.innerHTML = "";
-
 detailed_item.subtopics.forEach(item => {
-
 subtopics_container.appendChild(SubTopic(item))
 })
 
