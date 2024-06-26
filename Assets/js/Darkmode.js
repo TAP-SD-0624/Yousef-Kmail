c0a7de2f0db2b6620491f99c8c56df880b17615f
yousefkmail <yousef.hnaty@gmail.com>
2024-06-26 11:44:36 +0300

    feat: add docs to Darkmode.js

--- Assets/js/Darkmode.js
+++ Assets/js/Darkmode.js
@@ -1,3 +1,4 @@
+//toggling the class name of the body would change all variables used accross the app.
 export const ToggleThemeMode = () => {
   document.body.classList.contains("dark-mode")
     ? document.body.classList.remove("dark-mode")
