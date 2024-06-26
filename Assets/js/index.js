f82dd5a49b90d69b8f5b3fa5dded8946d1a91164
yousefkmail <yousef.hnaty@gmail.com>
2024-06-26 11:45:46 +0300

    feat: Add docs to the index.js

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -2,9 +2,11 @@ import data from "../../topics.json" with { type: "json" };
 import { ToggleThemeMode } from "./Darkmode.js";
 import { CreateCard } from "./Card.js";
 
+//Populate the cards info into the cards container in the main page.
 let cards_container = document.getElementById("cards-container");
+data.forEach(item => { cards_container.appendChild(CreateCard(item))})
 
-  data.forEach(item => { cards_container.appendChild(CreateCard(item))})
 
-  document.getElementById("theme-toggle").addEventListener("click" , ToggleThemeMode)
+//Add Dark mode toggle functionality to its respective button in the details page.
+document.getElementById("theme-toggle").addEventListener("click" , ToggleThemeMode)
 
