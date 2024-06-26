a28909f737e56aca525bcee9a24efd374834cdcc
yousefkmail <yousef.hnaty@gmail.com>
2024-06-26 11:48:45 +0300

    feat: add docs to card.js

--- Assets/js/Card.js
+++ Assets/js/Card.js
@@ -1,4 +1,7 @@
 import { GenerateStars } from "./StarsGenerator.js";
+
+//Generating card HTML and populate its data into its
+//respective HTML.
 export const CreateCard = ({ image, category, rating, name, topic, id }) => {
   let card = document.createElement("a");
   card.href = `./details.html?id=${id}`;
