c8a5c1d85637e4a2a03a8d5dcd7bbaa3f69bcd5b
yousefkmail <yousef.hnaty@gmail.com>
2024-06-30 11:11:50 +0300

    feat: make favorite card pressable

--- Assets/js/FavoriteCard.js
+++ Assets/js/FavoriteCard.js
@@ -1,9 +1,9 @@
 import { GenerateStars } from "./StarsGenerator.js";
 
-export const FavoriteCard = ({ image, topic, rating }) => {
-  let card = document.createElement("div");
+export const FavoriteCard = ({ image, topic, rating, id }) => {
+  let card = document.createElement("a");
   card.classList.add("favorite-card");
-
+  card.href = `./details.html?id=${id}`;
   let img = document.createElement("img");
   img.classList.add("favorite-card-img");
   img.src = `./Assets/Logos/${image}`;
